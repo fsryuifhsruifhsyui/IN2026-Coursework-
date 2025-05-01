db8b47d49505cec74f749d38d3c11706ae8e7e40
Hamidur-Rahman <hamidur.rahman.2@city.ac.uk>
2025-05-01 11:13:31 +0100

    Bug fix where asteroid and heart collide

--- SRC/Asteroid.cpp
+++ SRC/Asteroid.cpp
@@ -21,7 +21,7 @@ Asteroid::~Asteroid(void)
 
 bool Asteroid::CollisionTest(shared_ptr<GameObject> o)
 {
-	if (GetType() == o->GetType()) return false;
+	if (GetType() == o->GetType() || o->GetType() == GameObjectType("BonusLife")) return false;
 	if (mBoundingShape.get() == NULL) return false;
 	if (o->GetBoundingShape().get() == NULL) return false;
 	return mBoundingShape->CollisionTest(o->GetBoundingShape());
